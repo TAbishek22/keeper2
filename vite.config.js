4f8359a4a50380927ba9b9f7dfdf0f0c00ceaded
techfiesta <abithiruarani@gmail.com>
2024-07-27 22:50:36 +0530

    second44

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    base: "/keeper2/", // Replace <repository-name> with the actual name of your repository
+    plugins: [react()],
+});
